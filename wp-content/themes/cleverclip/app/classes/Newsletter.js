a94cbb93308fd683f88abb0adccaf5d31834f06b
Nico Brühlmann <bruehlmann@novu.ch>
2020-03-04 15:50:14 +0100

    update newsletter gender toggle value

--- wp-content/themes/cleverclip/app/classes/Newsletter.js
+++ wp-content/themes/cleverclip/app/classes/Newsletter.js
@@ -1,71 +1,74 @@
-import Alert from 'classes/Alert'
-import Loader from 'classes/Loader'
+import Alert from "classes/Alert";
+import Loader from "classes/Loader";
 
-import { post } from 'utils/ajax'
+import { post } from "utils/ajax";
 
 export default class {
   constructor({ button, classes, form, input }) {
-    this.button = button
-    this.classes = classes
-    this.form = form
-    this.input = input
+    this.button = button;
+    this.classes = classes;
+    this.form = form;
+    this.input = input;
 
-    this.addEventListeners()
+    this.addEventListeners();
   }
 
-  onSubmit (event) {
-    event.preventDefault()
+  onSubmit(event) {
+    event.preventDefault();
 
-    this.input.classList.remove(this.classes.input)
+    this.input.classList.remove(this.classes.input);
 
-    const data = new FormData(this.form)
-    const params = new URLSearchParams(data).toString()
+    const data = new FormData(this.form);
+    const params = new URLSearchParams(data).toString();
 
-    this.loader = new Loader()
+    this.loader = new Loader();
 
-    post(`/wp-json/contact-form-7/v1/contact-forms/${this.form.dataset.id}/feedback`, params).then(response => {
-      const { invalidFields, message = '', status } = JSON.parse(response)
+    post(
+      `/wp-json/contact-form-7/v1/contact-forms/${this.form.dataset.id}/feedback`,
+      params
+    ).then(response => {
+      const { invalidFields, message = "", status } = JSON.parse(response);
 
       if (invalidFields) {
-        this.input.classList.add(this.classes.input)
+        this.input.classList.add(this.classes.input);
       }
 
-      if (status === 'mail_sent') {
-        this.form.reset()
+      if (status === "mail_sent") {
+        this.form.reset();
 
-        const { successDescription, successTitle } = this.form.dataset
+        const { successDescription, successTitle } = this.form.dataset;
 
         new Alert({
           description: successDescription,
           title: successTitle
-        })
+        });
 
-        ga('send', 'event', 'Newsletter', 'Signup')
-      } else if (status === 'mail_failed') {
-        const { errorDescription, errorTitle } = this.form.dataset
+        ga("send", "event", "Newsletter", "Signup");
+      } else if (status === "mail_failed") {
+        const { errorDescription, errorTitle } = this.form.dataset;
 
         new Alert({
           description: errorDescription,
           title: errorTitle,
-          type: 'error'
-        })
+          type: "error"
+        });
       }
 
-      this.loader.hide()
-    })
+      this.loader.hide();
+    });
   }
 
-  addEventListeners () {
-    this.onSubmitEvent = this.onSubmit.bind(this)
+  addEventListeners() {
+    this.onSubmitEvent = this.onSubmit.bind(this);
 
-    this.button.addEventListener('click', this.onSubmitEvent)
+    this.button.addEventListener("click", this.onSubmitEvent);
   }
 
-  removeEventListeners () {
-    this.button.removeEventListener('click', this.onSubmitEvent)
+  removeEventListeners() {
+    this.button.removeEventListener("click", this.onSubmitEvent);
   }
 
-  destroy () {
-    this.removeEventListeners()
+  destroy() {
+    this.removeEventListeners();
   }
 }
