240b1d0ada0e13992e7a27ea5298e5f2d8017bda
guillaumecleverclip <guillaumecerdeira@cleverclip.ch>
2020-03-24 14:21:16 +0100

    Condition correction

--- remote-work-guide-fur-unternehmen/script.js
+++ remote-work-guide-fur-unternehmen/script.js
@@ -112,8 +112,8 @@ for(let i = 0; i < nav__categories.length; i ++){
                 categorie.classList.remove('active')
             }else{
                 guideNav.style.height = 'initial'
-                console.log(guideContent.clientHeight,guide.clientHeight)
-                if(guideNav.clientHeight == guide.clientHeight){
+                console.log(guideContent.clientHeight == guide.clientHeight)
+                if(guideContent.clientHeight == guide.clientHeight){
                     guideContent.style.height = parseInt(guideContent.clientHeight) - parseInt(guideNav.clientHeight)+'px'
                 }
                 for(let i = 0; i < nav__questions.length; i ++){
