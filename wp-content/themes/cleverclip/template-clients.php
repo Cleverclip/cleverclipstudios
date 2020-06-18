5dd365c6ad9d832f20f32d51f0edba8fcaf5979d
huba <huba.gaspar@gravityrd.com>
2020-06-18 14:21:21 +0200

    [development] clients page, added case studies grouped by service, de-duplicated case studies on the page

--- wp-content/themes/cleverclip/template-clients.php
+++ wp-content/themes/cleverclip/template-clients.php
@@ -4,48 +4,82 @@
 $context = Timber::get_context();
 
 $context['home'] = new TimberPost(
-  get_option('page_on_front')
+    get_option('page_on_front')
 );
 
 $context['post'] = new TimberPost();
+
+// cases, grouped by industries
 
 $context['industries'] = array();
 
 $industries = get_terms('industry');
 
-foreach ($industries as $industry) {
-  $case = Timber::get_posts(array(
-    'meta_key' => 'stars',
-    'order' => 'DESC',
-    'orderby' => 'meta_value',
-    'post_type' => 'case',
-    'posts_per_page' => 1,
-    'tax_query' => array(
-      array(
-        'taxonomy' => 'industry',
-        'field' => 'term_id',
-        'terms' => $industry->term_id,
-      )
-    )
-  ))[0];
+shuffle($industries);
 
-  array_push($context['industries'], $case);
+$industries = array_slice($industries, 0, 3);
+
+foreach ($industries as $industry) {
+    $case = Timber::get_posts(array(
+        'meta_key' => 'stars',
+        'order' => 'DESC',
+        'orderby' => 'meta_value',
+        'post_type' => 'case',
+        'posts_per_page' => 1,
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'industry',
+                'field' => 'term_id',
+                'terms' => $industry->term_id,
+            )
+        )
+    ))[0];
+
+    array_push($context['industries'], $case);
 }
 
-shuffle($context['industries']);
+// case study ids already on page
+$case_ids_on_page = array_map(function ($case_obj) {
+    return $case_obj->ID;
+}, $context["industries"]);
 
-$context['industries'] = array_slice($context['industries'], 0, 3);
+// cases, grouped by services (solutions)
 
-$context['solutions'] = Timber::get_posts(array(
-  'meta_key' => 'stars',
-  'order' => 'DESC',
-  'orderby' => 'meta_value',
-  'post_type' => 'case',
-  'posts_per_page' => 10
-));
+$context['solutions'] = array();
 
-shuffle($context['solutions']);
+$services = get_terms('service');
 
-$context['solutions'] = array_slice($context['solutions'], 0, 3);
+foreach ($services as $service) {
+    $cases = Timber::get_posts(array(
+        'meta_key' => 'stars',
+        'order' => 'DESC',
+        'orderby' => 'meta_value',
+        'post_type' => 'case',
+        'posts_per_page' => 5,
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'service',
+                'field' => 'term_id',
+                'terms' => $service->term_id,
+            )
+        )
+    ));
+
+    if (sizeof($cases) > 0) {
+        $with_most_stars = [];
+        foreach ($cases as $case) {
+            if (!in_array($case->ID, $case_ids_on_page)) {
+                if (sizeof($with_most_stars) === 0 || strlen($case->stars) === strlen($with_most_stars[0]->stars)) {
+                    array_push($with_most_stars, $case);
+                }
+            }
+        }
+
+        if (sizeof($with_most_stars) > 0) {
+            shuffle($with_most_stars);
+            array_push($context['solutions'], $with_most_stars[0]);
+        }
+    }
+}
 
 Timber::render('pages/clients.twig', $context);
